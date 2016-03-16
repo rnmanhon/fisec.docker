8c75f110488dd7019ba314d363341608c2c3822c
Raymond Ng <raymond.mh.ng@pccw.com>
2016-03-16 15:09:27 +0800

    test pep

---
+++ data/pep/fiware-pep-proxy/lib/idm.js
@@ -0,0 +1,120 @@
+var config = require('../config.js'),
+    proxy = require('./HTTPClient.js');
+
+var log = require('./logger').logger.getLogger("IDM-Client");
+
+var IDM = (function() {
+
+    var my_token,
+        //{token: {user_info: {}, date: Date}}
+        tokens_cache = {};
+
+    var authenticate = function(callback, callbackError) {
+
+        var options = {
+            host: config.keystone_host,
+            port: config.keystone_port,
+            path: '/v3/auth/tokens',
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}
+        };
+        var body = {
+            auth: {
+                identity: {
+                    methods: ['password'], 
+                    password: {
+                        user: {
+                            name: config.username, 
+                            password: config.password, 
+                            domain: {id: "default"}
+                        }
+                    }
+                }
+            }
+        };
+        proxy.sendData('http', options, JSON.stringify(body), undefined, function (status, resp, headers) {
+            my_token = headers['x-subject-token'];
+            callback(my_token);
+        }, callbackError);
+    };
+
+    var check_token = function(token, callback, callbackError) {
+
+        var options;
+
+        if (config.tokens_engine === 'keystone') {
+            options = {
+                host: config.keystone_host,
+                port: config.keystone_port,
+                path: '/v3/auth/tokens/',
+                method: 'GET',
+                headers: {
+                    'X-Auth-Token': my_token,
+                    'X-Subject-Token': encodeURIComponent(token),
+                    'Accept': 'application/json'
+                }
+            };
+
+        } else {
+            options = {
+                host: config.keystone_host,
+                port: config.keystone_port,
+                path: '/v3/access-tokens/' + encodeURIComponent(token),
+                method: 'GET',
+                headers: {'X-Auth-Token': my_token, 'Accept': 'application/json'}
+            };
+        }
+        
+        if (tokens_cache[token]) {
+            log.info('Token in cache, checking timestamp...');
+            var current_time = (new Date()).getTime();
+            var token_time = tokens_cache[token].date.getTime();
+
+            if (current_time - token_time < config.chache_time * 1000) {
+                tokens_cache[token].date = new Date();
+                callback(tokens_cache[token].user_info);
+                return;
+            } else {
+                log.info('Token in cache expired');
+                delete tokens_cache[token];
+            }
+        }
+        
+        log.info('Checking token with IDM...');
+
+        proxy.sendData('http', options, undefined, undefined, function (status, resp) {
+            var user_info = JSON.parse(resp);
+            tokens_cache[token] = {};
+            tokens_cache[token].date = new Date();
+            tokens_cache[token].user_info = user_info;
+            console.log(JSON.stringify(user_info));
+            callback(user_info);
+        }, function (status, e) {
+            if (status === 401) {
+
+                log.error('Error validating token. Proxy not authorized in keystone. Keystone authentication ...');   
+                authenticate (function (status, resp) {
+
+                    my_token = JSON.parse(resp).access.token.id;
+
+                    log.info('Success authenticating PEP proxy. Proxy Auth-token: ', my_token);
+                    check_token(token, callback, callbackError);
+
+                }, function (status, e) {
+                    log.error('Error in IDM communication ', e);
+                    callbackError(503, 'Error in IDM communication');
+                });
+            } else {
+                callbackError(status, e);
+            }
+        });
+    };
+
+
+    return {
+        authenticate: authenticate,
+        check_token: check_token
+    }
+
+})();
+exports.IDM = IDM;
